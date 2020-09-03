d4f3bd38269e35bb13dbc25b23c443d662d8f692
SIWAT CHAROENSONG <68978682+siwat-cha@users.noreply.github.com>
2020-09-04 00:44:17 +0700

    Update index.js
    HEADER_HEIGHT

--- src/expandableCalendar/index.js
+++ src/expandableCalendar/index.js
@@ -32,7 +32,7 @@ const BOUNCINESS = 6;
 const CLOSED_HEIGHT = 120; // header + 1 week
 const WEEK_HEIGHT = 46;
 const KNOB_CONTAINER_HEIGHT = 20;
-const HEADER_HEIGHT = 68;
+const HEADER_HEIGHT = 65;
 const DAY_NAMES_PADDING = 24;
 
 /**
